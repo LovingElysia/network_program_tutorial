07561682426b7b8949a7d1685f428e74189741c1
Eden <3397436863@qq.com>
2025-12-31 22:35:09 +0800

    添加项目文件。

---
+++ Hellosocket/Hellosocket/Test.cpp
@@ -0,0 +1,17 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include<Windows.h>
+#include<WinSock2.h>
+
+//#pragma coment(lib,"ws2_32.lib")    WSAStartup会调用一个动态库,所以，你必须包含，但这是仅仅适用于windows方法。 通用方法请在IDE中包含依赖
+
+
+int main()
+{
+	WORD ver{ MAKEWORD(2,2) }; //版本号
+	WSADATA data;
+	WSAStartup(ver, &data);    //开启套接字库
+
+	WSACleanup();       //关闭套接字库
+	return 0;
+}
